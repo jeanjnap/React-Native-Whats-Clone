7487465b17e862293d1e8c4d0be99c4cb6fb5443
JeanJNAP <jeanjnap@gmail.com>
2018-04-27 15:53:27 -0300

    Criando o componente FormLogin parte 2
    https://www.udemy.com/desenvolvedor-multiplataforma-androidios-com-react-e-redux/learn/v4/t/lecture/6852818?start=0

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
 export default props => (
-    <View>
-        <View>
-            <Text>WhatsApp Clone</Text>
+    <View style={{ flex: 1, padding: 10 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 25 }}>WhatsApp Clone</Text>
         </View>
-        <View>
-            <TextInput placeholder='E-mail' />
-            <TextInput placeholder='Senha' />
-            <Text>Ainda não tem cadastro? Cadastre-se</Text>
+        <View style={{ flex: 2}}>
+            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
+            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
+            <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
         </View>
-        <View>
-            <Button title="Acessar" onPress={() => false} />
+        <View style={{ flex: 2}}>
+            <Button title="Acessar" color='#115E54' onPress={() => false} />
         </View>
     </View>
 );
