6f6a1130210559f045c55c78ef424dc8a5bf5411
JeanJNAP <jeanjnap@gmail.com>
2018-11-27 18:37:07 -0200

    FormCadastro - Alterando os estados do componente via ActionCreators - parte 1
    https://www.udemy.com/desenvolvedor-multiplataforma-androidios-com-react-e-redux/learn/v4/t/lecture/6768732?start=0

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import { View, TextInput, Button } from 'react-native';
 import { connect } from 'react-redux';
+import { modificaEmail, modificaSenha, modificaNome } from '../actions/AutenticacaoActions';
 
-const cadastroForm = props => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={{ flex: 4, justifyContent: 'center' }}>
-            <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }} />
-            <TextInput value={props.email} placeholder="E-mail" style={{ fontSize: 20, height: 45 }} />
-            <TextInput value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }} />
+const formCadastro = props => {
+    console.log(props);
+    
+    return (
+        <View style={{ flex: 1, padding: 10 }}>
+            <View style={{ flex: 4, justifyContent: 'center' }}>
+                <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaNome(texto)} />
+                <TextInput value={props.email} placeholder="E-mail" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaEmail(texto)} />
+                <TextInput value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaSenha(texto)} />
+            </View>
+            <View style={{ flex: 1 }}>
+                <Button title="Cadastrar" color="#115E54" onPress={() => false} />
+            </View>
         </View>
-        <View style={{ flex: 1 }}>
-            <Button title="Cadastrar" color="#115E54" onPress={() => false} />
-        </View>
-    </View>
-);
+    );
+}
 
-const mapStateToProps = state => {
-
+const mapStateToProps = state => { 
     console.log(state);
+    
     return (
         {
             nome: state.AutenticacaoReducer.nome,
             email: state.AutenticacaoReducer.email,
             senha: state.AutenticacaoReducer.senha
         }
-    )
-};
+    );
+}
 
-export default connect(mapStateToProps, null)(cadastroForm);
+export default connect(mapStateToProps, { modificaEmail, modificaSenha, modificaNome })(formCadastro);
