d66b471d398f9642fb2d977b1a566258226ac32d
JeanJNAP <jeanjnap@gmail.com>
2018-11-28 10:06:18 -0200

    Instalando o firebase
    "firebase": "^4.13.1"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';
 
-export default props => (
-    <Provider store={createStore(reducers)}>
-        <Routes />
-    </Provider>
-);
+console.disableYellowBox = true;
+
+class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBrVuAtFlsH6GZ9zewlqqK5iyzlDynZwes",
+            authDomain: "whatsapp-clone-a1e00.firebaseapp.com",
+            databaseURL: "https://whatsapp-clone-a1e00.firebaseio.com",
+            projectId: "whatsapp-clone-a1e00",
+            storageBucket: "whatsapp-clone-a1e00.appspot.com",
+            messagingSenderId: "600196849959"
+        });
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <Routes />
+            </Provider>
+        );
+    }
+}
+
+export default App;
