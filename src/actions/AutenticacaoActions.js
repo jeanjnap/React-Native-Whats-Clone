f355fb02e56a47acbd9ff4fd3c9b7ba6ca53ba5f
JeanJNAP <jeanjnap@gmail.com>
2018-11-29 11:05:36 -0200

    realtime database salvar nome de usuário

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
+import b64 from 'base-64';
 
 export const modificaEmail = (texto) => {
     return {
@@ -25,7 +26,14 @@ export const modificaNome = (texto) => {
 export const cadastraUsuario = ({ nome, email, senha }) => {
     return dispatch => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
-            .then(user => cadastroUsuarioSucesso(dispatch))
+            .then(user => {
+                let emailB64 = b64.encode(email);
+                
+                firebase.database().ref('/contatos/'+emailB64)
+                    .push({ nome })
+                    .then(value => cadastroUsuarioSucesso(dispatch))
+                    .catch(erro => console.log(erro))
+            })
             .catch(erro => cadastroUsuarioErro(erro, dispatch));
     }
     
